e5247c65a5f33f780142278785ddd5fafd86dc7f
bochain <bochainwu@gmail.com>
2023-01-28 16:07:31 +0800

    remove  bio when read article

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -28,7 +27,7 @@ const BlogPostTemplate = ({
         />
         <hr />
         <footer>
-          <Bio />
+         
         </footer>
       </article>
       <nav className="blog-post-nav">
