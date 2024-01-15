0009b00146564f46db338144c85313d56ef7e24d
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-01-15 09:53:11 +0800

    style: change icon

--- src/components/seo.js
+++ src/components/seo.js
@@ -35,7 +35,7 @@ const Seo = ({ description, title, children }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="og:image" content="src/images/pic.jpeg"></meta>
+      <meta name="og:image" content="src/images/seo.jpg"></meta>
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:creator"
@@ -43,7 +43,7 @@ const Seo = ({ description, title, children }) => {
       />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
-      <meta name="twitter:image" content="src/images/pic.jpeg"></meta>
+      <meta name="twitter:image" content="src/images/seo.jpg"></meta>
       {children}
     </>
   )
