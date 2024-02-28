3e5e4c439fd01a96e9f9eae62f47be1590cd3c00
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-02-28 23:52:02 +0800

    feat: custom article layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
-
+import '@/styles/prism-plus.css'
+import '@/styles/prism-solarized-dark-atom.css'
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,49 @@
 import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
-import ThemeSwitch from '@/components/ThemeSwitch';
-import { allPostsNewToOld, Post } from '@/lib/contentLayerAdapter';
+import PostList, { PostForPostList } from '@/components/PostList';
+import { allPostsNewToOld } from '@/lib/contentLayerAdapter';
 
-export function getStaticProps() {
-  const posts = allPostsNewToOld;
-  return { props: { posts } };
-}
+import { headerConfigs } from '@/configs/headerConfigs';
+
+type PostForIndexPage = PostForPostList;
 
 type Props = {
-  posts: Post[];
+  posts: PostForIndexPage[];
+};
+
+export const getStaticProps: GetStaticProps<Props> = () => {
+  const posts = allPostsNewToOld.map((post) => ({
+    slug: post.slug,
+    date: post.date,
+    title: post.title,
+    description: post.description,
+    path: post.path,
+  })) as PostForIndexPage[];
+  return { props: { posts } };
 };
 
 const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div>
       <Head>
-        <title>My blog</title>
+        <title>{headerConfigs.title}</title>
         <meta name="description" content="Welcome to my blog" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white p-4 text-black dark:bg-black dark:text-white">
-        <h1 className="mb-6 text-4xl font-bold">Welcome to my blog!</h1>
+      <div className="prose my-12 space-y-2 transition-colors dark:prose-dark md:prose-lg md:space-y-5">
+        
+      </div>
 
-        <div className="my-4">
-          <ThemeSwitch />
+      <div className="my-4 divide-y divide-gray-200 transition-colors dark:divide-gray-700">
+        <div className="prose prose-lg my-8 dark:prose-dark">
+          <h2>最新文章</h2>
         </div>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          {posts.map((post) => (
-            <div
-              key={post.slug}
-              className="rounded-lg border border-black p-6 dark:border-white"
-            >
-              <a href={post.path}>
-                <h2 className="mb-4 text-2xl font-semibold">{post.title}</h2>
-                <p>{post.description}</p>
-              </a>
-            </div>
-          ))}
-        </div>
-      </main>
+        <PostList posts={posts} />
+      </div>
     </div>
   );
 };
