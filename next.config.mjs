b5d58576505c7507724b3e0b6bf1eefd26b77597
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-02-28 22:52:07 +0800

    fix: remove <a> use <Link legacyBehavior>

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ import { withContentlayer } from 'next-contentlayer';
 const nextConfig = withContentlayer({
   reactStrictMode: true,
   swcMinify: true,
+  // 加入以下 custom webpack 設定
+  // Support svg import
+  // ref: https://dev.to/dolearning/importing-svgs-to-next-js-nna
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  // 加入以上 custom webpack 設定
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -1,26 +1,20 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
+import Link from 'next/link'
+import type { LinkProps } from 'next/link'
+import { AnchorHTMLAttributes } from 'react'
 
-import Link from 'next/link';
-
-type Props = React.ComponentPropsWithoutRef<'a'>;
-
-const CustomLink = ({ href, ...rest }: Props) => {
-  const isInternalLink = href && href.startsWith('/');
-  const isAnchorLink = href && href.startsWith('#');
+const CustomLink = ({ href, ...rest }: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  const isInternalLink = href && href.startsWith('/')
+  const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    );
+    return <Link href={href} {...rest} />
   }
 
   if (isAnchorLink) {
-    return <a href={href} {...rest} />;
+    return <a href={href} {...rest} />
   }
 
-  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />;
-};
+  return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+}
 
-export default CustomLink;
+export default CustomLink
