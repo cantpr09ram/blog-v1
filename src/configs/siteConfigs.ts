4308c5260ebf8bcd7692e34c2af582c9733027cb
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-03-02 20:30:32 +0800

    feat: add seo matadata

---
+++ src/configs/siteConfigs.ts
@@ -0,0 +1,18 @@
+const fqdn = 'https://cantpr09ram.github.io/';
+const logoPath = '';
+const bannerPath = '';
+
+export const siteConfigs = {
+  title: 'Cantpr09ram Blog',
+  titleShort: 'My Blog',
+  description:
+    'A website which waste your time',
+  author: 'Me',
+  fqdn: fqdn,
+  logoPath: logoPath,
+  logoUrl: fqdn + logoPath,
+  bannerPath: bannerPath,
+  bannerUrl: fqdn + bannerPath,
+  twitterID: '@cantpr09ram',
+  datePublished: '2022-09-01',
+};
